03a9ec5f3b9ff5c3a90c4b7f28231091e0520102
allibrdoine <allibrodine81@gmail.com>
2022-10-16 14:11:00 -0500

    add queries and mutations to front end

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,61 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+    mutation login($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+                email
+            }
+        }
+    }`;
+
+export const ADD_USER = gql`
+    mutation addUser($username: String!, $password: String!, $email: String!) {
+        addUser(username: $username, password: $password, email: $email) {
+            token
+            user { 
+                _id
+                username
+                email
+            }
+        }
+    }`;
+
+export const SAVE_BOOK = gql`
+    mutation saveBook($bookData: BookInput!) {
+        saveBook(bookData: $bookData) {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                boodId
+                title
+                authors
+                description
+                image
+                link
+            }
+        }
+    }`;
+
+export const DELETE_BOOK = gql`
+    mutation deleteBook(bookId: ID!) {
+        removeBook(bookId: $bookId) {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                bookId
+                title 
+                authors
+                description
+                image
+                link
+            }
+        }
+    }`;
